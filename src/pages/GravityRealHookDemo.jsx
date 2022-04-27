a3d3b53d6a19aaca44091c9056e12cd5d5a4c1c2
Masao Kitamura <masaok@gmail.com>
2022-04-26 20:49:19 -0700

    Working on library npm dist fixes

--- src/pages/GravityRealHookDemo.jsx
+++ src/pages/GravityRealHookDemo.jsx
@@ -1,14 +1,14 @@
-// import useGravitySensor from 'react-hook-gravity'
+import useGravitySensor from 'react-hook-gravity'
 
 const GravityRealHookDemo = props => {
-  // const sensor = useGravitySensor({ frequency: 2 })
-  // console.log(sensor)
-  // return (
-  //   <div>
-  //     <div>useGravitySensor Real Hook Demo</div>
-  //     <pre>{JSON.stringify(sensor, null, 2)}</pre>
-  //   </div>
-  // )
+  const sensor = useGravitySensor({ frequency: 2 })
+  console.log(sensor)
+  return (
+    <div>
+      <div>useGravitySensor Real Hook Demo</div>
+      <pre>{JSON.stringify(sensor, null, 2)}</pre>
+    </div>
+  )
 }
 
 export default GravityRealHookDemo
--- src/pages/MagnetometerRealHookDemo.jsx
+++ src/pages/MagnetometerRealHookDemo.jsx
@@ -1,14 +1,14 @@
-import useMagnetometer from 'react-hook-magnetometer'
+// import useMagnetometer from 'react-hook-magnetometer'
 
-const MagnetometerRealHookDemo = props => {
-  const sensor = useMagnetometer({ frequency: 2 })
-  console.log(sensor)
-  return (
-    <div>
-      <div>useMagnetometer Real Hook Demo</div>
-      <pre>{JSON.stringify(sensor, null, 2)}</pre>
-    </div>
-  )
-}
+// const MagnetometerRealHookDemo = props => {
+//   const sensor = useMagnetometer({ frequency: 2 })
+//   console.log(sensor)
+//   return (
+//     <div>
+//       <div>useMagnetometer Real Hook Demo</div>
+//       <pre>{JSON.stringify(sensor, null, 2)}</pre>
+//     </div>
+//   )
+// }
 
-export default MagnetometerRealHookDemo
+// export default MagnetometerRealHookDemo
